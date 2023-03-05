196acc9cda8a0ad46ad05e88397f363664ecd4db
Allison Husain <allison.husain@berkeley.edu>
2023-03-05 01:28:30 -0800

    change CSRs to be S-mode so that the kernel can wield them

--- src/main/scala/common/mte_defs.scala
+++ src/main/scala/common/mte_defs.scala
@@ -39,26 +39,27 @@ object MTECSRs {
     All registers are allocated from the custom machine RW space
     (Table 2.1: Allocation of RISC-V CSR address ranges.)
     */
-    def mmte_configID   = 0x7c2
-    def mmte_faID       = 0x7c3
-    def mmte_fpcID      = 0x7c4
-    def mmte_tag_seedID = 0x7c5
+    def smte_configID   = 0x5c0
+    def smte_faID       = 0x5c1
+    def smte_fpcID      = 0x5c2
+    def smte_tag_seedID = 0x5c3
 
-    private val mmte_tag_base_min_id = mmte_tag_seedID + 1
-    def mmte_tagBaseMaskCount = 3
-    def mmte_tagbaseIDs:Seq[Int] = 0.to(mmte_tagBaseMaskCount).map {
+    private val smte_tag_base_min_id = smte_tag_seedID + 1
+    //TODO: Plumb the tag base/mask out to a broader config
+    def smte_tagBaseMaskCount = 3
+    def smte_tagbaseIDs:Seq[Int] = 0.to(smte_tagBaseMaskCount).map {
         i =>
-        mmte_tag_base_min_id + (2 * i)
+        smte_tag_base_min_id + (2 * i)
     }
-    def mmte_tagmaskIDs:Seq[Int] = 0.to(mmte_tagBaseMaskCount).map {
+    def smte_tagmaskIDs:Seq[Int] = 0.to(smte_tagBaseMaskCount).map {
         i =>
-        mmte_tag_base_min_id + (2 * i) + 1
+        smte_tag_base_min_id + (2 * i) + 1
     }
 
-    def mmte_config_enableShift         =   0
-    def mmte_config_enableMask          =   0x7
-    def mmte_config_enforceSyncShift    =   3
-    def mmte_config_enforceSyncMask     =   0x7
-    def mmte_config_permissiveTagShift  =   6
-    def mmte_config_permissiveTagMask   =   0xf
+    def smte_config_enableShift         =   0
+    def smte_config_enableMask          =   0x7
+    def smte_config_enforceSyncShift    =   3
+    def smte_config_enforceSyncMask     =   0x7
+    def smte_config_permissiveTagShift  =   6
+    def smte_config_permissiveTagMask   =   0xf
 }
